969ec0f055924aae64064cddb7819d48532b97c2
AminaChakirDev <amina.aitm@gmail.com>
2023-04-06 11:39:00 +0200

    carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'carousel01';
+  
+  scrollToRight(e: any, carousel: any, carouselImg: any) {
+    carousel.scrollLeft += carouselImg.width + 14;
+  }
+
+  scrollToLeft(e: any, carousel: any, carouselImg: any) {
+    carousel.scrollLeft -= carouselImg.width + 14;
+  }
 }
